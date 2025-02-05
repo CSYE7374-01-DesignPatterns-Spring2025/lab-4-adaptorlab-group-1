056b96ede980a4f391c3c31b69ecb7779d479cc3
yogitha-adapa13 <144865096+yogitha-adapa13@users.noreply.github.com>
2025-02-04 20:23:30 -0500

    added interface

--- src/main/java/edu/neu/csye7374/CalculatableAPI.java
+++ src/main/java/edu/neu/csye7374/CalculatableAPI.java
@@ -3,4 +3,5 @@ package edu.neu.csye7374;
 public interface CalculatableAPI {
     enum OPERATION { ADD, SUB, MULT, DIV }
     double operation(OPERATION op, double a, double b);
+    //calculatableapi
 }
