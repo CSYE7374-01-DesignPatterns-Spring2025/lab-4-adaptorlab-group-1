6366580b083b8227eb9f2491e35f10c498e502ab
PRASHANTH BASKAR <144980061+prashanthbaskar97@users.noreply.github.com>
2025-02-04 20:25:18 -0500

    Update Calculator.java
    Adding changes

--- src/main/java/edu/neu/csye7374/Calculator.java
+++ src/main/java/edu/neu/csye7374/Calculator.java
@@ -4,6 +4,7 @@ public class Calculator implements CalculatableAPI {
     @Override
     public double operation(OPERATION op, double a, double b) {
         switch(op) {
+                //Arithmetic Operations
             case ADD: return a + b;
             case SUB: return a - b;
             case MULT: return a * b;
